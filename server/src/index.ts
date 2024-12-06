ce72bc7c6a8936d35596992dcb56e64e3e6f1a50
Tobias Williams <towillia@redhat.com>
2024-12-06 16:35:50 -0500

    Update schema to reflect new values

--- server/src/index.ts
+++ server/src/index.ts
@@ -8,42 +8,71 @@ const mocks = {
   Query: () => ({
     tracksForHome: () => [...new Array(8)],
     hero: () => new Object,
+    social: () => new Object,
+    card: () => new Object,
+    topics: () => new Object,
   }),
 
   Track: () => ({
-    id: () => "uuid0001",
-    title: () => "Nullam Condimentum Elit Purus",
+    learningResourceId: () => "uuid0001",
+    learningResourceAlternativeHeading: () => "Nullam Condimentum Elit Purus",
+    learningResourcePosition: () => 5,
     author: () => {
       return {
-        name: "Dr.L.Path",
-        url:
-          "https://bing.com",
+        authorName: "Dr.L.Path",
+        authorUrl: "https://bing.com",
       };
     },
-    summary: () =>
+    learningResourceIntroduction: () =>
       "Curabitur blandit tempus porttitor. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Donec id elit non mi porta gravida at eget metus.",
-    body: () => "Nulla vitae elit libero, a pharetra augue. Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Maecenas faucibus mollis interdum. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Nullam quis risus eget urna mollis ornare vel eu leo. Donec ullamcorper nulla non metus auctor fringilla.Nullam id dolor id nibh ultricies vehicula ut id elit.Maecenas faucibus mollis interdum.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nulla vitae elit libero, a pharetra augue. Lorem ipsum dolor sit amet, consectetur adipiscing elit.Nullam quis risus eget urna mollis ornare vel eu leo.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Aenean lacinia bibendum nulla sed consectetur.Maecenas faucibus mollis interdum.Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.Nulla vitae elit libero, a pharetra augue.Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.Morbi leo risus, porta ac consectetur ac, vestibulum at eros.Donec ullamcorper nulla non metus auctor fringilla.",
-    length: () => "5 minutes",
+    learningResourceTimeToComplete: () => "5 minutes",
+    learningResourceEducationalLevel: () => "Intermediate"
   }),
 
   HeroContent: () => ({
-    title: () => "Get Started with RHEL",
-    description: () => "Welcome to Red Hat. As a new user or even a longtime user of Red Hat products, it's important to keep your account information, user profile, and community profile up-to-date. Learn how to access your information, and understand the benefits of personalizing your preferences. ",
-    cta: () => {
-      return {
-        text: "Download RHEL",
-        url: "https://bing.com",
-      };
-    },
-    media: () => "https://placehold.co/600x400",
+    heroHeading: () => "Get Started with RHEL",
+    heroDescription: () => "Welcome to Red Hat. As a new user or even a longtime user of Red Hat products, it's important to keep your account information, user profile, and community profile up-to-date. Learn how to access your information, and understand the benefits of personalizing your preferences. ",
     metadata: () => {
       return {
         type: "Learning Path",
-        resourceCount: 8,
-        time: "5 minutes",
-        publishedDate: "April 26th 1992"
+        numberOfLearningResources: 8,
+        timeToComplete: "5 minutes",
+        datePublished: "April 26th 1992"
+      };
+    },
+    topics: () => {
+      return {
+        topicId: "Learning",
+        topicName: "dc188-1213213",
+        topicUrl: "https://bing.com"
       };
     }
+  }),
+
+  SocialShare: () => ({
+    shareImageUrl: () => "https://placehold.co/600x400",
+    shareImageAlt: () => "Get Started with RHEL"
+  }),
+
+  CardPreso: () => ({
+    featureImageUrl: () => "https://placehold.co/600x400",
+    featureImageAlt: () => "Get Started with RHEL",
+    description: () => "Donec ullamcorper nulla non metus auctor fringilla.",
+    body: () => "Maecenas sed diam eget risus varius blandit sit amet non magna. Maecenas sed diam eget risus varius blandit sit amet non magna. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec ullamcorper nulla non metus auctor fringilla.",
+    metadata: () => {
+      return {
+        type: "Learning Path",
+        numberOfLearningResources: 8,
+        timeToComplete: "5 minutes",
+        datePublished: "April 26th 1992"
+      };
+    }
+  }),
+
+  Topics: () => ({
+    topicId: () => "dc188-1213213",
+    topicName:  () => "Products",
+    topicUrl: () => "https://bing.com"
   }),
 };
 
--- server/src/schema.ts
+++ server/src/schema.ts
@@ -11,42 +11,59 @@ export const typeDefs = gql`
     hero: HeroContent
   }
 
+  type Query {
+    "Get hero for learning path landing page"
+    social: SocialShare
+  }
+
+  type Query {
+    card: CardPreso
+  }
+
+  type Query {
+    topics: Topics
+  }
 
   "A learning path is a group of resources that teaches about a specific topic"
   type Track {
-    id: ID!
-    title: String!
-    author: Author!
-    "This is a summary of the resource for card presentations"
-    summary: String
-    "This is the main body of the content"
-    body: String
+    learningResourceId: ID!
+    learningResourcePosition: Int
+    learningResourceAlternativeHeading: String
+    learningResourceIntroduction: String
+    learningResourceEducationalLevel: String
     "Measured in time, presented as a string ie 5minutes"
-    length: String
+    learningResourceTimeToComplete: String
+    author: Authors
   }
 
   "Author of a complete learning path or a resource"
-  type Author {
-    id: ID!
-    name: String!
-    url: String
+  type Authors {
+    authorName: String!
+    authorUrl: String
   }
 
   "Hero content for learning path"
   type HeroContent {
-    title: String!
-    description: String
-    cta: CTA
-    media: String
+    heroHeading: String!
+    heroDescription: String
+    body: String
     metadata: MetaData
+    topics: Topics
   }
 
-  "A cta for the hero"
-  type CTA {
-    "The text displayed in the cta"
-    text: String
-    "Destination of the cta"
-    url: String
+  "Social Share Info"
+  type SocialShare {
+    shareImageUrl: String
+    shareImageAlt: String
+  }
+
+  "Information for card presentation of LP"
+  type CardPreso {
+    featureImageUrl: String
+    featureImageAlt: String
+    description: String
+    body: String
+    metadata: MetaData
   }
 
   "Meta data for the hero"
@@ -54,11 +71,17 @@ export const typeDefs = gql`
     "Type will likely be 'learning path"
     type: String
     "How many resources are in the list"
-    resourceCount: Int
+    numberOfLearningResources: Int
     "How long the path takes, stored as a string"
-    time: String
+    timeToComplete: String
     "Date published, stored as a string"
-    publishedDate: String
+    datePublished: String
   }
 
+  "Topics of the path"
+  type Topics {
+    topicId: String
+    topicName: String
+    topicUrl: String
+  }
 `;
